40d98f92902ffc66b4f147e5c8c7d66388443033
WalterJin2 <walterjin2@gmail.com>
2023-02-25 12:23:26 +1100

    Add authController authRoutes loginLimiter to do login & refresh token rotation

---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const authController = require('../controllers/authController')
+const loginLimiter = require('../middleware/loginLimiter')
+
+router.route('/')
+    .post(loginLimiter, authController.login)
+
+router.route('/refresh')
+    .get(authController.refresh)
+
+module.exports = router
