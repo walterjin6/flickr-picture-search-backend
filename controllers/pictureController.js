406b862fb98f57a3af0ddd66efde701914c85373
WalterJin2 <walterjin2@gmail.com>
2023-02-26 11:33:10 +1100

    Update getPicturePrivate with new Flickr api

--- controllers/pictureController.js
+++ controllers/pictureController.js
@@ -41,8 +41,11 @@ const getPicturePublic = async (req, res) => {
 }
 
 const getPicturePrivate = async (req, res) => {
-  const tags = req.body.input.input
-  const flickrHome = `https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1&tags=${tags}`
+   console.log("好啊好啊")
+  console.log(req.body)
+
+  const tags = req.body.input
+  const flickrHome = `http://api.flickr.com/services/rest/?format=json&sort=random&method=flickr.photos.search&tags=${tags}&tag_mode=all&api_key=${process.env.FLICKR_API_KEY}&nojsoncallback=1`
   let data = await getData()
   let items = []
   async function getData() {
@@ -55,30 +58,31 @@ const getPicturePrivate = async (req, res) => {
     }
   }
 
-  if (data) {
-    for (let i = 0; i < data.items.length; i++) {
-      let obj = {
-        id: i + 1,
-        title: data.items[i].title,
-        url: data.items[i].media.m,
-      }
-      items.push(obj)
-    }
-    var response = {
-      status: 'Success',
-      code: 200,
-      data: {
-        searchTitle: data.title,
-        items: items,
-      },
-    }
-  } else {
-    var response = {
-      status: 'Error',
-      code: 400,
-    }
-  }
-  return res.json(response)
+   const photos = data?.photos?.photo
+   if (photos?.length > 0) {
+     photos.forEach(function (photo) {
+       items.push({
+         id: photo.id,
+         url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
+         title: photo.title,
+       })
+     })
+
+     var response = {
+       status: 'Success',
+       code: 200,
+       data: {
+         searchTitle: 'Looking for image?',
+         items: items,
+       },
+     }
+   } else {
+     var response = {
+       status: 'Error',
+       code: 400,
+     }
+   }
+   return res.json(response)
 }
 
 module.exports = {
