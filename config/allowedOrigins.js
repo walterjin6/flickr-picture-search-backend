0ead561c18bffd58549f293814885b310f1cd0fe
WalterJin2 <walterjin2@gmail.com>
2023-03-15 19:41:32 +1100

    update readme.md

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,8 @@
 const allowedOrigins = [
-    'http://localhost:3000',
+  'http://localhost:3000',
+  'http://localhost:2998',
+  'http://localhost:2999',
+  'https://flickrpicturesearch.onrender.com',
 ]
 
 module.exports = allowedOrigins
